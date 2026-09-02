93dd4e2817ddaada87de16c8078ad4abe776ccf5
shrey9455 <131531200+shrey9455@users.noreply.github.com>
2026-09-02 12:03:19 +0530

    Time: 0 ms (100%), Space: 12.4 MB (65%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -4,10 +4,4 @@ class Solution(object):
         :type nums1: List[int]
         :rtype: bool
         """
-        even=[i for i in nums1 if i%2==0]
-        odd=[i for i in nums1 if i%2!=0]
-        if len(even):
-            return True
-        if len(odd):
-            return True
-        return False
+        return True
