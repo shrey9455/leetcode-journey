871ae64501a529e03345db2a8c08161553f68b46
shrey9455 <131531200+shrey9455@users.noreply.github.com>
2026-09-05 14:07:45 +0530

    Time: 6 ms (82.72%), Space: 12.3 MB (60.49%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
@@ -5,7 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        for i in range(len(nums)):
-            if (max(nums[:i+1])-min(nums[i:]))<=k:
+        n=len(nums)
+        mn=float('inf')
+        suffix=[0]*n
+        for i in range(n-1,-1,-1):
+            mn=min(mn,nums[i])
+            suffix[i]=mn
+        mx=0
+        for i in range(n):
+            mx=max(mx,nums[i])
+            if mx-suffix[i]<=k:
                 return i
+
         return -1
